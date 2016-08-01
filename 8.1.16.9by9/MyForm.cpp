84a5c59a98d1b049c2d6edec6ad78a81e29aba8e
mark52551 <mark.ruiz5@gmail.com>
2016-08-01 18:01:38 -0400

    first commit

---
+++ 8.1.16.9by9/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	_8_1_16_9by9::MyForm form;
+	Application::Run(%form);
+}
---
+++ 8.1.16.9by9/MyForm.h
@@ -0,0 +1,123 @@
+#pragma once
+
+namespace _8_1_16_9by9 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+			
+
+	protected:
+	private: System::Windows::Forms::Button^  button1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(13, 13);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(419, 394);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(462, 172);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(576, 439);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+		
+	}
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		Bitmap^ bmp = gcnew Bitmap(L"helpme.bmp");
+		Graphics^ g = pictureBox1->CreateGraphics();
+		//g->DrawImage(bmp, 40, 0);
+		Pen^ pen = gcnew Pen(Color::Blue);
+		int offset = 40;
+		int i;
+		for (i = 0; i < 10; i++)
+		{
+			g->DrawLine(pen, 40, i*offset, 400, i*offset);
+			g->DrawLine(pen, 40 + i*offset, 0, 40 + i*offset, 9 * offset);
+		}
+		
+		g->DrawImage(bmp, 40, 40);
+		g->DrawImage(bmp, 80, 80);
+		g->DrawImage(bmp, 120, 80);
+		g->DrawImage(bmp, 160, 120);
+		g->DrawImage(bmp, 200, 160);
+		g->DrawImage(bmp, 240, 200);
+
+	}
+	
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	};
+}
